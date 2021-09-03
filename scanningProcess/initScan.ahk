e642446edcadd36b77d37c8e8ce35fab252c3e08
pixustechnologies <helpdesk@pixustechnologies.com>
2021-09-03 14:35:09 -0400

    scanner key change comments for printWIP

--- scanningProcess/initScan.ahk
+++ scanningProcess/initScan.ahk
@@ -4,8 +4,8 @@ SendMode Input  ; Recommended for new scripts due to its superior speed and reli
 SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
 
 
-; ^ means the control key, f10 is f10
-; could be changed to another f key, ideally 8 9 or 10
+; ^ means the control key, f9 is f9
+; could be changed to another f key, ideally 8 or 9
 ; ^ is to ensure the user wants to enter the program
 ^F9::
     	Run, logCheck.exe
--- scanningProcess/printWIP.bat
+++ scanningProcess/printWIP.bat
@@ -1,16 +1,13 @@
 @echo off
 set orderNum=%1
 
-:: simple loop
-set /a "index = 1"
-:while1
-if %index% leq %printNum% (
-	C:\Program Files\Millet Software\DataLink Viewer 2011\DataLink_Viewer_2011.exe" -v "C:\temp\Report.rpt" "user_id:dba" "password:sql" "Parm1:%orderNum%" "Printer:\\Srv1\Small2"
-	C:\Program Files\Millet Software\DataLink Viewer 2011\DataLink_Viewer_2011.exe" -v "C:\temp\Report.rpt" "user_id:dba" "password:sql" "Parm1:%orderNum%" "Printer:\\Srv1\Large1"
-	echo Printing Number %index%
-	set /a "index = index + 1"
-	goto :while1
-)
+:: the BOM
+C:\Program Files\Millet Software\DataLink Viewer 2011\DataLink_Viewer_2011.exe" -v "C:\temp\Report.rpt" "user_id:dba" "password:sql" "Parm1:%orderNum%" "Printer:\\Srv1\Small2"
+:: the configureation report
+C:\Program Files\Millet Software\DataLink Viewer 2011\DataLink_Viewer_2011.exe" -v "C:\temp\Report.rpt" "user_id:dba" "password:sql" "Parm1:%orderNum%" "Printer:\\Srv1\Large1"
+:: the list of serial numbers 
+C:\Program Files\Millet Software\DataLink Viewer 2011\DataLink_Viewer_2011.exe" -v "C:\temp\Report.rpt" "user_id:dba" "password:sql" "Parm1:%orderNum%" "Printer:\\Srv1\Large1"
+
 echo.
 echo Finished Printing
 echo.
